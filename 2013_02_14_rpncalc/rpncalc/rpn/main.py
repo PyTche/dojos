aa4231d65d02e1f0aa0aac7c4cae034f47dbebec
Christian S. Perone <christian.perone@gmail.com>
2013-02-14 22:17:33 -0200

    Commit inicial.

---
+++ 2013_02_14_rpncalc/rpncalc/rpn/main.py
@@ -0,0 +1,38 @@
+import operator
+
+
+class RPNCalc(object):
+
+	OPS = {
+		'+' : operator.add,
+		'-' : operator.sub,
+		'*' : operator.mul,
+		'/' : operator.div,
+	}
+
+	def __init__(self):
+		self.stack = []
+	
+	def eval(self, expr):
+		tokens = expr.split()
+		for token in tokens:
+			if token not in RPNCalc.OPS:
+				try:
+					value = float(token)
+				except ValueError:
+					raise NotImplementedError('Operator {} not'
+						' implemented !'.format(token)) 
+				self.stack.append(token)
+			else:
+				rhs, lhs = self.stack.pop(), self.stack.pop()
+				op = RPNCalc.OPS[token]
+				self.stack.append(op(float(lhs), float(rhs)))
+
+		return self.stack.pop()
+
+
+
+
+
+
+
---
+++ 2013_02_14_rpncalc/rpncalc/rpn/tests/test_rpn.py
@@ -0,0 +1,41 @@
+from __future__ import division
+import unittest
+from rpn import RPNCalc
+
+class TestRPN(unittest.TestCase):
+
+	def setUp(self):
+		self.calc = RPNCalc()
+
+	def test_add_2(self):
+		self.assertEqual(self.calc.eval('2 2 +'), 2+2)
+		self.assertEqual(self.calc.eval('2 3 +'), 2+3)
+
+	def test_add_3(self):
+		self.assertEqual(self.calc.eval('2 3 + 2 +'), 2+3+2)
+
+	def test_sub(self):
+		self.assertEqual(self.calc.eval('3 2 -'), 3-2)
+
+	def test_mul(self):
+		self.assertEqual(self.calc.eval('3 2 *'), 3*2)
+
+	def test_div(self):
+		self.assertEqual(self.calc.eval('4 2 /'), 4/2)
+	
+	def test_inf(self):
+		self.assertEqual(self.calc.eval('+inf -inf -'), float("+inf"))
+
+	def test_negative(self):
+		self.assertEqual(self.calc.eval('-20 2 +'), -20+2)
+
+	def test_final(self):
+		expr = '3 2 / 1.5 + 2 - 4 *'
+		self.assertEqual(self.calc.eval(expr), ((((3/2)+1.5)-2)*4))
+
+	def test_not_implemented(self):
+		with self.assertRaises(NotImplementedError):
+		    self.calc.eval('3 2 @')
+
+if __name__ == "__main__":
+	unittest.main()
