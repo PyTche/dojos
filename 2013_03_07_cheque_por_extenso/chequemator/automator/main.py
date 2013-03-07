aa828489c6506e36bb6bed31101b885638072cbe
Filipe Cifali <cifali.filipe@gmail.com>
2013-03-07 20:56:53 -0300

    Update no projeto

--- 2013_03_07_cheque_por_extenso/chequemator/automator/main.py
+++ 2013_03_07_cheque_por_extenso/chequemator/automator/main.py
@@ -1,46 +1,83 @@
 class Automator(object):
-	
-	VALORES={
-		1:'um',
-		2:'dois',
-		3:'tres',
-		4:'quatro',
-		5:'cinco',
-		6:'seis',
-		7:'sete',
-		8:'oito',
-		9:'nove'
-		}
-		
-	VALORES_DEZENA={
-		10:'dez',
-		20:'vinte',
-		30:'trinta',
-		40:'quarenta',
-		50:'cinquenta',
-		60:'sessenta',
-		70:'setenta',
-		80:'oitenta',
-		90:'noventa'
-		}
-		
-	VALORES_CENTENA={
-		100:'cem',
-		200:'duzentos',
-		300:'trezentos',
-		400:'quatrocentos',
-		500:'quinhentos',
-		600:'seiscentos',
-		700:'setecentos', 
-		800:'oitocentos',
-		900:'novecentos'
-		} 
-	
-	def __init__(self):
-		self.valor = 0
-		
-	def translate(self, valor):
-		if valor in Automator.VALORES:
-			return Automator.VALORES[valor]
-		
-		
+
+    VALORES={
+        1:'um',
+        2:'dois',
+        3:'tres',
+        4:'quatro',
+        5:'cinco',
+        6:'seis',
+        7:'sete',
+        8:'oito',
+        9:'nove'
+        }
+
+    VALORES_11_19={
+        11:'onze',
+        12:'doze',
+        13:'treze',
+        14:'quatorze',
+        15:'quinze',
+        16:'dezesseis',
+        17:'dezessete',
+        18:'dezoito',
+        19:'dezenove'
+        }
+
+    VALORES_DEZENA={
+        10:'dez',
+        20:'vinte',
+        30:'trinta',
+        40:'quarenta',
+        50:'cinquenta',
+        60:'sessenta',
+        70:'setenta',
+        80:'oitenta',
+        90:'noventa'
+        }
+
+    VALORES_CENTENA={
+        100:'cem',
+        200:'duzentos',
+        300:'trezentos',
+        400:'quatrocentos',
+        500:'quinhentos',
+        600:'seiscentos',
+        700:'setecentos',
+        800:'oitocentos',
+        900:'novecentos'
+        }
+
+    def __init__(self):
+        self.valor = 0
+
+    def translate(self, valor):
+        result = ''
+        s_p = '0'
+        s_m = '0'
+        s_u = '0'
+
+        if len(str(valor)) > 2:
+            s_p = str(valor)[0]+'00'
+            c = str(valor)[1:]
+        else:
+            c = str(valor)
+
+        if int(s_p) in Automator.VALORES_CENTENA:
+            result += Automator.VALORES_CENTENA[int(s_p)]
+            result += ' e '
+
+        if int(c) > 10 and int(c) < 20:
+            result += Automator.VALORES_11_19[int(str(valor)[1:])]
+        else:
+            s_m = str(valor)[-2:1]+'0'
+            s_u = str(valor)[1:]
+
+        if int(s_m) in Automator.VALORES_DEZENA:
+            result += Automator.VALORES_DEZENA[int(s_m)]
+            result += ' e '
+
+        if int(s_u) in Automator.VALORES:
+            result += Automator.VALORES[int(s_u)]
+
+        return result
--- 2013_03_07_cheque_por_extenso/chequemator/automator/tests/test_automator.py
+++ 2013_03_07_cheque_por_extenso/chequemator/automator/tests/test_automator.py
@@ -2,13 +2,22 @@ import unittest
 from automator import Automator
 
 class TestAutomator(unittest.TestCase):
-	
-	def setUp(self):
-		self.auto = Automator()
-		
-	def test_menor_de_dez(self):
-		self.assertEqual(self.auto.translate(5),'cinco')
-	
-	def test_dezena(self):
-		self.assertEqual(self.auto.translate(15),'quinze')
-		
+
+    def setUp(self):
+        self.auto = Automator()
+
+    def test_menor_de_dez(self):
+        self.assertEqual(self.auto.translate(5),'cinco')
+
+    def test_11_19(self):
+        self.assertEqual(self.auto.translate(15),'quinze')
+
+    def test_dezena_2(self):
+        self.assertEqual(self.auto.translate(53),'cinquenta e tres')
+        self.assertEqual(self.auto.translate(87),'oitenta e sete')
+
+    def test_centena(self):
+        self.assertEqual(self.auto.translate(356),'trezentos e cinquenta e seis')
+
+    def test_centena_11_19(self):
+        self.assertEqual(self.auto.translate(519), 'quinhentos e dezenove')
