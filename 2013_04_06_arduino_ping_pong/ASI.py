749f8d2171abca9b064b0492f65ee37924cf79a1
Filipe Cifali <cifali.filipe@gmail.com>
2013-04-06 15:26:13 -0300

    Update, add function to the main loop

--- 2013_04_06_arduino_ping_pong/ASI.py
+++ 2013_04_06_arduino_ping_pong/ASI.py
@@ -9,16 +9,18 @@ Created by Filipe Cifali Stangler on 2013-04-06.
 import serial
 import sys
 
-def main():
-	if sys.argv is Null:
-		print "No data input.\n Input data for the Arduino"
+def main(argv):
+	if len(argv) < 2:
+		print "No data input.\n Input data for the Arduino.\n Usage:",argv[0],"<argv[1]>"
+		return 1
 	else:
 		baud_rate = 9600
-		s = serial.Serial('/dev/tts.usbmodem1411',baud_rate)
-		s.write(sys.argv[1])
-		print "Data inputed!!\n Data: ->",sys.argv[1],"\nCheck your Arduino!!"
+		s = serial.Serial('/dev/tty.usbmodem1411',baud_rate)
+		s.write(argv[1])
+		print "Data inputed!!\nData: ->",argv[1],"\nCheck your Arduino!!"
 		s.close()
+		return 1
 
 if __name__ == '__main__':
-	main()
+	main(sys.exit(main(sys.argv)))
 
